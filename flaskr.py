cd04e4ade3aba7955b976c45e252c9fe99d662cf
hannielfls <hannielfls@gmail.com>
2021-06-18 17:27:44 +0000

    primeiro commit

---
+++ flaskr.py
@@ -0,0 +1,24 @@
+import sqlite3
+from flask import Flask
+
+# Configurações
+
+DATABASE = './flaskr.db'
+SECRET_KEY = "pudim"
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+# Aplicação
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+
+
+
