fe0b213e5464e2b03523149fb1c195d4ec783a13
hannielfls <hannielfls@gmail.com>
2021-06-18 19:03:08 +0000

    Aula Ocean

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, g, render_template, request
 
 # Configurações
 
@@ -16,9 +16,25 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(DATABASE)
 
+
+@app.before_request
+def before():
+    g.db = connect_db()
+
+@app.teardown_request
+def after(exception):
+    g.db.close()
+
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    sql = 'SELECT titulo, texto from entradas order by id desc'
+    cur = g.db.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cur.fetchall()]
+    return render_template('index.html', entradas=entradas)
 
-
-
+@app.route('/inserir', methods=['POST'])
+def inserir():
+    sql = "INSERT INTO entradas(titulo, texto) values (?, ?)"
+    g.db.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.db.commit()
+    return render_template('index.html')
